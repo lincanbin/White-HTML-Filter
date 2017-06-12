e2c7a7d505f55ac19482b478ed81f3f0ebc16e92
lincanbin <lincanbin@hotmail.com>
2017-06-12 15:50:23 +0800

    Output XHTML / HTML5.

--- test.php
+++ test.php
@@ -52,14 +52,8 @@ $filter->config->WhiteListCssClass = array('contain', 'sider');
 
 
 //No filter
-if (version_compare(PHP_VERSION, '5.4.0') < 0) {
-    // PHP 5.3.0
-    test(
-        '<div class="contain"><span style="color: #f00;"><p>test中文</p>
-<br>line2</span></div>',
-        '<div class="contain"><span style="color:#f00;"><p>test中文</p>
-<br>line2</span></div>');
-} else {
+// PHP 5.4+
+if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
     test(
         '<div class="contain"><span style="color: #f00;"><p>test中文</p>
 <br/>line2</span></div>',
